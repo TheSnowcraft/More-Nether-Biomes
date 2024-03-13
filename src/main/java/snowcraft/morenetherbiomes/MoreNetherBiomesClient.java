fe1560070de4d2ccbdc22a6379b92ea855320790
TheSnowcraft <30834419+TheSnowcraft@users.noreply.github.com>
2024-03-13 15:41:22 +0000

    Initial commit

---
+++ src/main/java/snowcraft/morenetherbiomes/MoreNetherBiomesClient.java
@@ -0,0 +1,9 @@
+package snowcraft.morenetherbiomes;
+
+import net.fabricmc.api.ClientModInitializer;
+public class MoreNetherBiomesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/snowcraft/morenetherbiomes/MoreNetherBiomesDataGenerator.java
@@ -0,0 +1,11 @@
+package snowcraft.morenetherbiomes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreNetherBiomesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
